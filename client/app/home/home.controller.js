93b52c4b912feb03fe4f151c96e572bd054522f0
Colin Seale <ceseale3@gmail.com>
2015-11-23 16:36:14 -0800

    remove routeToEvent method

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -14,21 +14,10 @@
       //call the factory function to get all of the meals
       homeFactory.getMeals()
       .then(function(data) {
+        console.log(data); 
         self.events = data;
       });
       
-    };
-
-    self.routeToEvent = function() {
-
-      homeFactory.getEvent()
-      .then(function(data) {
-        console.log('hello');
-        //this badly written function is to manually routoe to the meail of id one(written in a last minute panic to try and get it working)
-        //it would be better to change the viw using ui-sref? in the view and then from 
-        //that controller call the function to get the correct data from the database!
-        $window.location.href = "/#/meals/1";
-      });
     };
 
     self.events = [];
