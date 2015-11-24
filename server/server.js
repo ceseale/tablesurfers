b65961f27fa073c22159c92dde103ab670565854
Soroush Mehraein <soroushmehraein@gmail.com>
2015-11-23 17:31:42 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,19 +17,6 @@ var mealsidRoutes = require('./routes/mealsIdRoutes');
 
 // configuration ===============================================================
 
-app.use(function(req, res, next) {
-res.header('Access-Control-Allow-Credentials', 'true');
-res.header('Access-Control-Allow-Origin', req.headers.origin);
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-if ('OPTIONS' == req.method) {
-     res.send(200);
- } else {
-     next();
- }
-});
-
-
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
